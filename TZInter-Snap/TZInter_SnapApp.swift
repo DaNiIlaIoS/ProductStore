b03557993b4479047c1e762d1b91d502e3314a52
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-08-25 12:59:03 +0500

    Initial Commit

---
+++ TZInter-Snap/TZInter_SnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TZInter_SnapApp.swift
+//  TZInter-Snap
+//
+//  Created by Даниил Сивожелезов on 25.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TZInter_SnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
